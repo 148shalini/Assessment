a2049b6c421d395da34c3fea26847d4429205020
148shalini <101161992+148shalini@users.noreply.github.com>
2024-08-08 21:19:32 +0530

    commit

--- student-profile/src/services/api.js
+++ student-profile/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'https://student-profile-backend.vercel.app/api/profile';
+const API_URL = 'http://localhost:5000/api/profile/api/profile';
 //http://localhost:5000/api/profile
 
 const getProfile = async () => {
