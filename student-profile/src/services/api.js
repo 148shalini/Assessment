ca3706fc52ee5fa4e5917bc47df4c1cec441eafb
148shalini <101161992+148shalini@users.noreply.github.com>
2024-08-08 16:15:11 +0530

    commit

---
+++ student-profile/src/services/api.js
@@ -0,0 +1,17 @@
+// src/services/api.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/profile';
+
+const getProfile = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+const updateProfile = async (newProfileData) => {
+  const response = await axios.put(API_URL, newProfileData);
+  return response.data;
+};
+
+export default { getProfile, updateProfile };
+//
